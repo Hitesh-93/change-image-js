9128b493c5fa24a354d0ae506b40510d0a591abd
unknown <you@example.com>
2022-07-02 20:09:06 +0530

    first commit

---
+++ csript.js
@@ -0,0 +1,18 @@
+
+function test() {
+
+    // document.getElementById("demo").innerHTML= Date();
+
+    document.getElementById("demo").innerHTML = "Good Morning";
+    document.getElementById("demo").style.color = 'dodgerblue';
+    document.getElementById("image").src = "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/1200";
+
+}
+
+
+function change() {
+
+    document.getElementById("demo").innerHTML = "Good Night";
+    document.getElementById("demo").style.color = 'black';
+    document.getElementById("image").src = "https://blog.nature.org/science/files/2022/03/2c1b520c2317143afcab18af2c026c46_original-scaled.jpg";
+}
